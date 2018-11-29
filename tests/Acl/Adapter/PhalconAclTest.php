f299876d117b4db23a6a9e6574fa27e03583a9a6
tempcke <t.github@logikostech.net>
2018-11-29 15:39:07 -0600

    no phalcon yet, just basic db connectivity tests to make sure the sqlite test db was working

--- tests/Acl/Adapter/PhalconAclTest.php
+++ tests/Acl/Adapter/PhalconAclTest.php
@@ -3,8 +3,28 @@
 namespace LogikosTest\Access\Acl\Adapter;
 
 use Logikos\Access\Acl\Adapter\PhalconAcl;
-use PHPUnit\Framework\TestCase;
+use LogikosTest\Access\Acl\TestCase;
 
 class PhalconAclTest extends TestCase {
+
+  public function setUp() {
+    parent::setUp();
+    $this->loadFixtures();
+  }
+
+  protected function loadFixtures() {
+    $this->addRoles('admin', 'member', 'guest');
+    $this->addResources('dashboard', 'reports', 'members');
+    $this->addPrivileges('dashboard', 'login');
+    $this->addPrivileges('reports', 'view', 'filter', 'schedule');
+    $this->addPrivileges('members', 'view', 'add', 'edit', 'suspend');
+    $this->addGrant('member', 'dashboard', 'login');
+  }
+
+  public function testCanWriteAndReadFromDb() {
+    $r = $this->db->selectFirst('roles', 'role', ['role'=>'admin']);
+    $this->assertEquals('admin', $r['role']);
+  }
+
 
 }
