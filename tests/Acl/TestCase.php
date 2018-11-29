ecae8c2d918474c1d7c0f1d571a07e9c316a6ed7
tempcke <t.github@logikostech.net>
2018-11-29 15:38:33 -0600

    base test cases for shared resources (database)

---
+++ tests/Acl/TestCase.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace LogikosTest\Access\Acl;
+
+use Exception;
+use Logikos\Database\Connection;
+use Logikos\Database\DbConfig;
+use LogikosTest\Access\Db;
+use LogikosTest\Access\TestCase as AccessTestCase;
+use Nette\Database\Helpers as DbHelpers;
+
+class TestCase extends AccessTestCase {
+  /** @var Connection */
+  protected $db;
+
+  public function setUp() {
+    $this->db = Db::connection();
+    DbHelpers::loadFromFile($this->db, self::DIR_TESTS.'/aclschema.sql');
+  }
+
+  protected function addRoles(...$roles) {
+    foreach ($roles as $r) {
+      $row = ['role' => $r];
+      $this->db->insert('roles', $row);
+    }
+  }
+
+  protected function addResources(...$resources) {
+    foreach ($resources as $r) {
+      $row = ['resource' => $r];
+      $this->db->insert('resources', $row);
+    }
+  }
+
+  protected function addPrivileges($resource, ...$privileges) {
+    foreach ($privileges as $r) {
+      $row = ['resource' => $resource, 'privilege' => $r];
+      $this->db->insert('privileges', $row);
+    }
+  }
+
+  protected function addGrant($role, $resource, ...$privileges) {
+    foreach ($privileges as $r) {
+      $row = ['role' => $role, 'resource' => $resource, 'privilege' => $r];
+      $this->db->insert('role_privileges', $row);
+    }
+  }
+
+  protected function inheritRoles($role, $inheritedRoles) {
+    foreach ($inheritedRoles as $r) {
+      $row = ['role' => $role, 'role_inherit' => $r];
+      $this->db->insert('role_inherits', $row);
+    }
+  }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace LogikosTest\Access;
+
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase {
+  const DIR_TESTS = __DIR__;
+}
