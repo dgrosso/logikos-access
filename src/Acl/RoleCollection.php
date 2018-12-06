6061d9d16a6058bc29e16a303541c4971523850a
tempcke <t.github@logikostech.net>
2018-12-06 07:19:02 -0600

    RoleCollection

--- src/Acl/RoleCollection.php
+++ src/Acl/RoleCollection.php
@@ -3,7 +3,21 @@
 
 namespace Logikos\Access\Acl;
 
+use Logikos\Access\Acl\Entity\Role as RoleEntity;
 
 class RoleCollection extends \IteratorIterator implements RoleIterator {
 
+  public static function buildFromPdoStatement(\PDOStatement $sth) {
+    return new static($sth);
+  }
+
+  public function current(): Role {
+    $row = parent::current();
+    $r = new RoleEntity([
+        'name' => $row['role'],
+        'description' => $row['description']
+    ]);
+    /** @var Role $r */
+    return $r;
+  }
 }
--- tests/Acl/RoleCollectionTest.php
+++ tests/Acl/RoleCollectionTest.php
@@ -4,9 +4,41 @@
 namespace LogikosTest\Access\Acl;
 
 
+use Logikos\Access\Acl\Role;
+use Logikos\Access\Acl\RoleCollection;
+use PHPUnit\Framework\Assert;
+
 class RoleCollectionTest extends TestCase {
-  public function testFoo() {
-    $this->markTestSkipped();
+  private $roles = ['admin', 'member', 'guest'];
+
+  public function setUp() {
+    parent::setUp();
+    $this->loadFixtures();
+  }
+
+  protected function loadFixtures() {
+    $this->addRoles(...$this->roles);
+  }
+
+  public function testBuildFromPdoStatement() {
+    $sth = $this->db->pdoQuery("select * from roles");
+    $collection = RoleCollection::buildFromPdoStatement($sth);
+
+    $found = [];
+    foreach ($collection as $role) {
+      Assert::assertInstanceOf(Role::class, $role);
+      array_push($found, $role->name());
+    }
+    Assert::assertEquals($this->roles, $found);
+  }
+
+  public function testBuildFromNetteResultSet() {
+    $sth = $this->db->selectWhere('roles', 'role, description', []);
+    $collection = new RoleCollection($sth);
+
+    foreach ($collection as $resource) {
+      Assert::assertInstanceOf(Role::class, $resource);
+    }
   }
 
 }
