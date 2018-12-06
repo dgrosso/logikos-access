170168191a741f1ecd5c45468a62fb335e75bbed
tempcke <t.github@logikostech.net>
2018-12-06 13:36:29 -0600

    added new assertion to compare arrays without caring about value order

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,15 @@
 namespace LogikosTest\Access;
 
 
+use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 class TestCase extends PHPUnitTestCase {
   const DIR_TESTS = __DIR__;
+
+  public static function assertArrayValuesEqual(array $expected, array $actual) {
+    sort($expected);
+    sort($actual);
+    Assert::AssertEquals($expected, $actual);
+  }
 }
