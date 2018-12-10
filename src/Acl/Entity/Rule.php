dd96f7e020e2236cd1a3195073a1ee81021835fe
tempcke <t.github@logikostech.net>
2018-12-10 15:37:26 -0600

    implemented Rule Entity

--- src/Acl/Entity/Rule.php
+++ src/Acl/Entity/Rule.php
@@ -4,24 +4,51 @@
 namespace Logikos\Access\Acl\Entity;
 
 
+use Logikos\Access\Acl;
 use Logikos\Access\Acl\Entity;
 use Logikos\Access\Acl\Rule as RuleInterface;
+use Logikos\Util\Config\Field\Field;
+use Logikos\Util\Validation\Validator\Callback;
 
 class Rule extends Entity implements RuleInterface {
 
   public function role() {
-    // TODO: Implement role() method.
+    return $this->get('role');
   }
 
   public function resource() {
-    // TODO: Implement resource() method.
+    return $this->get('resource');
   }
 
   public function privilege() {
-    // TODO: Implement privilege() method.
+    return $this->get('privilege');
   }
 
   public function access() {
-    // TODO: Implement access() method.
+    return $this->get('access');
+  }
+
+  protected function defaults(): array {
+    return [
+        'access' => Acl::ALLOW
+    ];
+  }
+
+  protected function initialize() {
+    $this->addFields(
+        new Field('role'),
+        new Field('resource'),
+        new Field('privilege'),
+        Field::withValidators('access',
+            new Callback(
+                [$this, 'isValidAccess'],
+                'Must pass Acl::ALLOW or Acl::DENY'
+            )
+        )
+    );
+  }
+
+  public function isValidAccess($value) {
+    return in_array($value, [Acl::ALLOW, Acl::DENY]);
   }
 }
--- tests/Acl/Entity/RuleTest.php
+++ tests/Acl/Entity/RuleTest.php
@@ -4,17 +4,35 @@
 namespace LogikosTest\Access\Acl\Entity;
 
 
+use Logikos\Access\Acl;
 use Logikos\Access\Acl\Entity\Rule;
 use LogikosTest\Access\Acl\TestCase;
 
 class RuleTest extends TestCase {
-  public function testFoo() {
+
+  public function testRequiredFields() {
     $r = new Rule();
-    $r->role();
-    $r->resource();
-    $r->privilege();
-    $r->access();
-    $this->assertFalse(true, 'start here');
+    $this->assertFieldValidationFailed($r, 'role');
+    $this->assertFieldValidationFailed($r, 'resource');
+    $this->assertFieldValidationFailed($r, 'privilege');
+  }
+
+  public function testRuleAccessDefaultIsAllowed() {
+    $r = new Rule();
+    $this->assertEquals(Acl::ALLOW, $r->access());
+  }
+
+  public function testCanGetData() {
+    $r = new Rule([
+        'role' => 'member',
+        'resource' => 'reports',
+        'privilege' => 'download'
+    ]);
+
+    $this->assertEquals('member', $r->role());
+    $this->assertEquals('reports', $r->resource());
+    $this->assertEquals('download', $r->privilege());
+    $this->assertTrue($r->isValid());
   }
 
 }
