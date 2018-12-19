047e6cec525818407334f0afa12e234d81efd6b7
tempcke <t.github@logikostech.net>
2018-12-19 15:51:16 -0600

    extracted generic entity collection tests into their own class, as these tests apply to any collection

---
+++ tests/Acl/EntityCollectionTest.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace LogikosTest\Access\Acl;
+
+use Logikos\Access\Acl\Role;
+use PHPUnit\Framework\Assert;
+
+class EntityCollectionTest extends TestCase {
+
+  public function testCanSerialize() {
+
+    /** @var Role\Role[] $data */
+    $data = [
+        'A' => Role\Role::build('A'),
+        'B' => Role\Role::build('B'),
+        'C' => Role\Role::build('E', ['A','B'])
+    ];
+
+    /** @var Role\Role[] $collection */
+    $collection = Role\Collection::fromArray($data);
+
+    Assert::assertEquals($collection, unserialize(serialize($collection)));
+  }
+
+
+  public function testBuildFromPdoStatement() {
+    $roles = ['admin', 'member', 'guest'];
+    $this->addRoles(...$roles);
+
+    $sth = $this->db->pdoQuery("select * from roles");
+    $collection = Role\Collection::fromPdoStatement($sth);
+
+    $found = [];
+
+    /** @var Role $role */
+    foreach ($collection as $role) {
+      Assert::assertInstanceOf(Role::class, $role);
+      array_push($found, $role->name());
+    }
+    Assert::assertEquals($roles, $found);
+  }
+
+  public function testConstructFromPdoStatementCanIterateMoreThanOnce() {
+    $roles = ['admin', 'member', 'guest'];
+    $this->addRoles(...$roles);
+
+    $sth = $this->db->pdoQuery("select * from roles");
+    $collection = new Role\Collection($sth);
+    $c1 = $c2 = 0;
+    foreach($collection as $r1) $c1++;
+    foreach($collection as $r2) $c2++;
+    Assert::assertGreaterThanOrEqual(1, $c1);
+    Assert::assertEquals($c1, $c2);
+  }
+
+  public function testBuildFromNetteResultSet() {
+    $roles = ['admin', 'member', 'guest'];
+    $this->addRoles(...$roles);
+
+    $sth = $this->db->selectWhere('roles', 'role, description', []);
+    $collection = new Role\Collection($sth);
+
+    foreach ($collection as $resource) {
+      Assert::assertInstanceOf(Role::class, $resource);
+    }
+  }
+}
--- tests/Acl/Role/RoleCollectionTest.php
+++ tests/Acl/Role/RoleCollectionTest.php
@@ -21,39 +21,6 @@ class RoleCollectionTest extends TestCase {
     $this->addRoles(...$this->roles);
   }
 
-  public function testBuildFromPdoStatement() {
-    $sth = $this->db->pdoQuery("select * from roles");
-    $collection = Collection::fromPdoStatement($sth);
-
-    $found = [];
-
-    /** @var Role $role */
-    foreach ($collection as $role) {
-      Assert::assertInstanceOf(Role::class, $role);
-      array_push($found, $role->name());
-    }
-    Assert::assertEquals($this->roles, $found);
-  }
-
-  public function testConstructFromPdoStatementCanIterateMoreThanOnce() {
-    $sth = $this->db->pdoQuery("select * from roles");
-    $collection = new Collection($sth);
-    $c1 = $c2 = 0;
-    foreach($collection as $r1) $c1++;
-    foreach($collection as $r2) $c2++;
-    Assert::assertGreaterThanOrEqual(1, $c1);
-    Assert::assertEquals($c1, $c2);
-  }
-
-  public function testBuildFromNetteResultSet() {
-    $sth = $this->db->selectWhere('roles', 'role, description', []);
-    $collection = new Collection($sth);
-
-    foreach ($collection as $resource) {
-      Assert::assertInstanceOf(Role::class, $resource);
-    }
-  }
-
 
   public function testBuildFromArray() {
     $data = [
@@ -119,19 +86,5 @@ class RoleCollectionTest extends TestCase {
     }
   }
 
-  public function testCanSerialize() {
-
-    /** @var Role\Role[] $data */
-    $data = [
-        'A' => Role\Role::build('A'),
-        'B' => Role\Role::build('B'),
-        'C' => Role\Role::build('E', ['A','B'])
-    ];
-
-    /** @var Role\Role[] $collection */
-    $collection = Collection::fromArray($data);
-
-    Assert::assertEquals($collection, unserialize(serialize($collection)));
-  }
 
 }
